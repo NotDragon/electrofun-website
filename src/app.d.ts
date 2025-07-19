14cbb82284ca8abfb52ec21799278d6cef6b4276
NotDragon <mixalis.m.hadjittofi@gmail.com>
2025-07-19 07:48:40 +0300

    Added Supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,19 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
+// See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import { SupabaseClient, User, Session } from '@supabase/supabase-js';
+
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Locals {
+			supabase: SupabaseClient;
+			safeGetSession: () => Promise<
+				| { session: null; user: null }
+				| { session: Session; user: User | null }
+			>;
+			user: User | null;
+			session: Session | null;
+		}
 	}
 }
 
